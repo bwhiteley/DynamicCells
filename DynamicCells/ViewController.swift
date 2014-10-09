bc9da7105d99575b989872119319b594e2ff024c
Bart Whiteley <bwhiteley@gmail.com>
2014-10-09 14:52:48 -0600

    Initial Commit

---
+++ DynamicCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicCells
+//
+//  Created by Bart Whiteley on 10/9/14.
+//  Copyright (c) 2014 Bart Whiteley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
